1a3733166774eb4423f9cef9fe0aafb4029e0c72
dmitry <dime10cents@gmail.com>
2016-12-26 23:24:36 +0600

    add seek method

--- 02_files/index.js
+++ 02_files/index.js
@@ -4,18 +4,22 @@ const pokemonsData = require('./data/pokemons');
 
 const COMMANDS = process.argv;
 
-if (COMMANDS.length < 3) {
+function help() {
   console.log('Попробуйте следующие команды:');
   console.log('node index hide ./path ./data.json - спрятать случайное число покемонов из файла data.json в директорию ./path');
   console.log('node index seek ./path - найти всех покемонов в директории ./path');
+}
+
+if (COMMANDS.length < 3) {
+  help();
 } else start();
 
 function start () {
+  let path = COMMANDS[3] || './field';
   if (COMMANDS[2] == 'hide') {
-    let path = COMMANDS[3] || './field';
     let data = (COMMANDS[4] ? require(COMMANDS[4]) : pokemonsData);
     hide(path, data);
   } else if (COMMANDS[2] == 'seek') {
-    seek();
-  }
+    seek(path);
+  } else help();
 }
--- 02_files/modules/hidenseek.js
+++ 02_files/modules/hidenseek.js
@@ -6,8 +6,9 @@ const {Pokemon, PokemonList} = require('./pokemonList');
 
 function createFolders(path='./field', count = 10) {
   return new Promise((resolve,reject)=> {
-    let folders = pathMod.normalize(path).split('\\');
+    let folders = pathMod.normalize(path).split('/');
     let fullPath = '.';
+    console.log(folders);
     folders.forEach((item)=> {
       if (item) {
         fullPath += `/${item}`
@@ -16,7 +17,7 @@ function createFolders(path='./field', count = 10) {
             console.log(err);
             reject(err);
           }
-        });  
+        });
       };
     });
     for (let i = 1; i <= count; i++) {
@@ -32,77 +33,88 @@ function createFolders(path='./field', count = 10) {
   });
 };
 
-function createPath(path) {
-    let folders = path.split('/');
-    return new Promise((resolve, reject) => {
-        let res = folders.reduce((path,folder)=> {
-            if (path == "." || path == "..") {
-              return path + '/' + folder;
-            };
-            fs.mkdirSync(path, (err)=> {
-              if(err) {
-                reject(err);
-              };
-            });
-            return path + '/' + folder;
-        });
-        resolve(res);
-    });
-};
-
 function twoDigit(num) {
     return (num < 10 ? `0${num}` : num)
 };
 
 function getRandomPokemon(list) {
-    let pokemonCount = list.length;
-    let randomNum = random(0, pokemonCount - 1);
-    let pokemon = list[randomNum];
-    return {
-        info:`${pokemon.name}|${pokemon.level}`,
-        pokemon: pokemon
-    }
+  let pokemonCount = list.length;
+  let randomNum = random(0, pokemonCount - 1);
+  let pokemon = list[randomNum];
+  return {
+      info:`${pokemon.name}|${pokemon.level}`,
+      pokemon: pokemon
+  }
 };
 
 function hidePokemon(pokemon, path) {
-    return new Promise((resolve,reject) => {
-        fs.appendFile(path + '/pokemon.txt', pokemon, (err) => {
-            if (err) {
-                reject(err);
-            }
-        });
+  return new Promise((resolve,reject) => {
+    fs.appendFile(path + '/pokemon.txt', pokemon, (err) => {
+      if (err) {
+        reject(err);
+      }
     });
+  });
 };
 
+function deepSeek(path) {
+  let pokemons = [];
+  return new Promise((resolve, reject)=> {
+    fs.readFile(path + '/pokemon.txt', 'utf-8', (err, data)=> {
+      if (err) {
+        fs.readdir(path, (err, folders)=> {
+          if (folders.length) {
+            folders.forEach((folder)=>{
+              deepSeek(path+'/'+folder);
+            })
+          }
+        });
+      } else {
+        console.log(data);
+        resolve();
+      }
+    });
+  });
+};
+// function outputPoks(data) {
+//   if (data.length) {
+//     console.log('---------found pokemons:');
+//     data.forEach((item) => {
+//       let pokemon = item.split('|');
+//       console.log(`name: ${pokemon[0]}`);
+//       console.log(`level: ${pokemon[1]}`);
+//     })
+//   } else {
+//     console.log('Pokemons are not found');
+//   }
+// }
+
 module.exports = {
-    hide: function(path, pokemonsData) {
-        let pokemonList = new PokemonList;
-        pokemonList.addAll(pokemonsData);
-        let hiddenPokemons = [];
-        createFolders(path, 10);
-        // createPath(path)
-        //     .then(makeDir(path))
-        //     .then(createFolders(path, 10))
-        //     .then((path)=> {
-        //         let countRandomPokemons = pokemonList.length < 3 ? random(1 , pokemonList.length) : random(1,3);
-        //         for (let i = 0 ; i<countRandomPokemons; i++) {
-        //             let {info, pokemon}  = getRandomPokemon(pokemonList);
-        //             while (hiddenPokemons.indexOf(pokemon) != -1) {
-        //               let randomPokemon = getRandomPokemon(pokemonList);
-        //               info = randomPokemon.info;
-        //               pokemon = randomPokemon.pokemon;
-        //             };
-        //             let randomFolder = twoDigit(random(1,10));
-        //             hiddenPokemons.push(pokemon);
-        //             hidePokemon(info, path + '/' + randomFolder);
-        //           ;
-        //         };
-        //         console.log('------Hidden Pokemons:');
-        //         hiddenPokemons.forEach(item => item.show());
-        //     })
-        //     .catch(err => console.log(err));
+  hide: function(path, pokemonsData) {
+    let pokemonList = new PokemonList;
+    pokemonList.addAll(pokemonsData);
+    let hiddenPokemons = [];
+    createFolders(path, 10)
+      .then((path)=> {
+        let countRandomPokemons = pokemonList.length < 3 ? random(1 , pokemonList.length) : random(1,3);
+        for (let i = 0 ; i<countRandomPokemons; i++) {
+          let {info, pokemon}  = getRandomPokemon(pokemonList);
+          while (hiddenPokemons.indexOf(pokemon) != -1) {
+            let randomPokemon = getRandomPokemon(pokemonList);
+            info = randomPokemon.info;
+            pokemon = randomPokemon.pokemon;
+          };
+          let randomFolder = twoDigit(random(1,10));
+          hiddenPokemons.push(pokemon);
+          hidePokemon(info, path + '/' + randomFolder);
+        };
+        console.log('------Hidden Pokemons:');
+        hiddenPokemons.forEach(item => item.show());
+      })
+      .catch(err => console.log(err));
   },
-  seek: function () {
-    console.log('seek');
+  seek: function (path) {
+    deepSeek(path)
+    .catch(err => console.log(err));
   }
 };
