0629dadafd25e319e95d635ea341e3b169399a54
dmitry <dime10cents@gmail.com>
2017-01-29 12:56:06 +0600

    express routing

---
+++ 07_express-routing/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extendded": true}));
+
+app.get('/', (req, res)=> {
+  res.status(200).send('Hello Express.js');
+});
+
+app.get('/hello', (req, res)=> {
+  res.status(200).send('Hello stranger!');
+});
+
+app.get('/hello/:name', (req, res)=> {
+  let name = req.params.name;
+  res.status(200).send(`Hello ${name}!`);
+});
+
+app.all('/sub/*', (req, res)=> {
+  let uri = req.url;
+  res.status(200).send(`You request URI: ${uri}`);
+});
+
+app.post('/post', middleware, (req, res)=> {
+  if (Object.keys(req.body).length) {
+    res.json(req.body);
+  };
+  res.sendStatus(404);
+});
+
+function middleware(req, res, next) {
+  if (req.get('Header') == 'key') {
+    return next();
+  };
+  res.sendStatus(401);
+};
+
+app.listen(3000, ()=> {
+  console.log('listening port 3000');
+})
